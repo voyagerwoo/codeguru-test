921e588cc9a3bc7d042f153374ed3d6522e5c23b
voyagerwoo <voyager.woo@gmail.com>
2020-01-20 03:46:23 +0900

    First commit

---
+++ src/main/java/vw/codegurutest/CodeguruTestApplication.java
@@ -0,0 +1,13 @@
+package vw.codegurutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeguruTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeguruTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vw/codegurutest/CodeguruTestApplicationTests.java
@@ -0,0 +1,13 @@
+package vw.codegurutest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeguruTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
